b6bfbf0fac59ddeb09b5350954c9fb301aca19e5
JeysonJosueGomezVillamizarADSI <aminoterlo@gmail.com>
2022-09-17 18:26:57 -0500

    Programa 1

---
+++ main.js
@@ -0,0 +1,9 @@
+let num = Number (prompt("Digite un numero"));
+
+if(num>0 && num<100){
+    document.write("<p class='resultado'>El numero es positivo y menor que 100"+"</p>");
+}
+else{
+    document.write("<p class='resultado'>El numero es negativo"+"</p>");
+}
+
