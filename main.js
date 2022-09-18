d5fb3d3ce1a60932f6c4a34bbfdfbc071a655509
JeysonJosueGomezVillamizarADSI <aminoterlo@gmail.com>
2022-09-18 14:49:06 -0500

    Programa 3

--- main.js
+++ main.js
@@ -2,12 +2,44 @@ let num1 = Number(prompt("Ingrese el primer numero : "));
 let num2 = Number(prompt("Ingrese el segundo numero : "));
 let num3 = Number(prompt("Ingrese el tercer numero : "));
 
-if(num1>num2 && num1>num3){
-    document.write("El numero mayor es " + num1);
-}else if (num2>num3 && num2>num1){
-    document.write("El numero mayor es "+ num2);
+if (num1 >= num2 && num1 > num3) {
+    if (num2 > num3) {
+        alert(num1 + 'es el mayor')
+        alert(num3 + 'es el menor');
+    }
+} else if (num3 > num2) {
+    alert(num1 + 'es el mayor')
+    alert(num2 + 'es el menor')
+} else {
+    alert(num1 + 'es el mayor')
+    alert('El segundo y el tercer numero son iguales' + num2)
 }
-else{
-    document.write("El numero mayor es " + num3)
+
+if (num2 > num1 && num2 >= num3)
+    if (num1 > num3) {
+        alert(num1 + 'es el mayor')
+        print(num3 + 'es el menor')
+    } else if (num3 > num2) {
+        alert(num2 + 'es el mayor')
+        alert(num1 + 'es el menor')
+    } else {
+        alert(num2 + 'es el mayor')
+        alert('El primer y el tercer numero son iguales' + num1)
+    }
+
+if (num3 >= num1 && num3 > num2) {
+    if (num1 > num2) {
+        alert(num3 + 'es el mayor')
+        alert(num2 + 'es el menor')
+    }
+} else if (b > a) {
+    alert(num3 + 'es el mayor')
+    print(num1 + 'es el menor')
+} else {
+    print(c, 'es el mayor')
+    print('El primer y el segundo numero son iguales', a)
 }
+
+
+
 
