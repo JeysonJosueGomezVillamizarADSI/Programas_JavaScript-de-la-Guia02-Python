318e1300afbffbad2341c493f29dbb20933c3351
JeysonJosueGomezVillamizarADSI <aminoterlo@gmail.com>
2022-09-18 22:04:02 -0500

    Programa 13

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-let numero1=Number(prompt("Escribe el primer numero "))
-let numero2=Number(prompt("Escribe el segundo numero "))
+let valor = Number(prompt("Ingrese el valor de la compra"));
+let tipMembresia = prompt("Digite el tipo de membresia que posee , Tipo A ,Tipo B o Tipo C");
 
-if (numero1>numero2){
-    resta=numero1- numero2
-    document.write("El resultado de la resta es " + resta)
+if(tipMembresia == "Tipo A"){
+    totPago1 = (valor) - (valor*0.10)
+    alert("El valor a pagar por poseer la membresia Tipo A es de " + totPago1);
+}else if(tipMembresia == "Tipo B"){
+    totPago2 = (valor) - (valor*0.15)
+    alert("El valor a pagar por poseer la membresia Tipo B es de " + totPago2);
+}else if(tipMembresia == "Tipo C"){
+    totPago3 = (valor) - (valor*0.20)
+    alert("El valor a pagar por poseer la membresia Tipo C es de " + totPago3);
 }else{
-    alert("La operacion no es posible ")
+    alert("El valor a pagar por no poseer ningun tipo de membresia es de " + valor);
 }
-    
