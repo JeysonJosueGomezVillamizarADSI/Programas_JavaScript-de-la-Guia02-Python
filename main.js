1aef8cc1fb6aa07ad1c8cbf689799894ed4dda60
JeysonJosueGomezVillamizarADSI <aminoterlo@gmail.com>
2022-09-18 21:52:36 -0500

    Programa 12

--- main.js
+++ main.js
@@ -1,33 +1,10 @@
-let nom1=(prompt("Ingrese el nombre del primer estudiante "))
-let ape1=(prompt("Ingrese el apellido del primer estudiante "))
-eda1=Number(prompt("Ingrese la edad del primer estudiante "))
-not1=Number(prompt("Ingrese la nota del primer estudiante "))
+let numero1=Number(prompt("Escribe el primer numero "))
+let numero2=Number(prompt("Escribe el segundo numero "))
 
-nom2=(prompt("Ingrese el nombre del segundo estudiante "))
-ape2=(prompt("Ingrese el apellido del segundo estudiente "))
-eda2=Number(prompt("Ingrese la edad del segundo estudiante "))
-not2=Number(prompt("Ingrese la nota del segundo estudiante "))
-
-nom3=(prompt("Ingrese el nombre del tercer estudiante "))
-ape3=(prompt("Ingrese el apellido del tercer estudiante "))
-eda3=Number(prompt("Ingrese la edad del tercer estudiante "))
-not3=Number(prompt("Ingrese la nota del tercer estudiante "))
-
-nom4=(prompt("Ingrese el nombre del cuarto estudiante "))
-ape4=(prompt("Ingrese el apellido del cuarto estudiante "))
-eda4=Number(prompt("Ingrese la edad del cuarto estudiante "))
-not4=Number(prompt("Ingrese la nota del cuarto estudiante "))
-
-nom5=(prompt("Ingrese el nombre del quinto estudiante "))
-ape5=(prompt("Ingrese el apellido del quinto estudiante "))
-eda5=Number(prompt("Ingrese la edad del quinto estudiante "))
-not5=Number(prompt("Ingrese la nota del quinto estudiante "))
-
-let promedio = (not1+not2+not3+not4+not5)/5;
-
-alert("El nombre del primer estudiante es " + nom1 + "El apellido del primer estudiante es" + ape1 + "La nota del primer estudiante es " + not1)
-alert("El nombre del segundo estudiante es " + nom2 + "El apellido del segundo estudiante es" + ape2 + "La nota del segundo estudiante es " + not2)
-alert("El nombre del tercer estudiante es " + nom3 + "El apellido del tercer estudiante es" + ape3 + "La nota del tercer estudiante es " + not3)
-alert("El nombre del cuarto estudiante es " + nom4 + "El apellido del caurto estudiante es" + ape4 + "La nota del cuarto estudiante es " + not4)
-alert("El nombre del quinto estudiante es " + nom5 + "El apellido del quinto estudiante es" + ape5 + "La nota del quinto estudiante es " + not5)
-alert("El promedio de la nota de los cinco estudiantes es" + prom)
+if (numero1>numero2){
+    resta=numero1- numero2
+    document.write("El resultado de la resta es " + resta)
+}else{
+    alert("La operacion no es posible ")
+}
+    
